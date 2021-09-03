883295d7558c5456772735c5ef9909345a721135
leonibres <leonibresjimenez@gmail.com>
2021-09-03 10:47:40 -0500

    Inicio del curso

---
+++ Javascript/02-01-TiposDeDatosJavascript.js
@@ -0,0 +1,61 @@
+//TODO: Ejemplo de tipos de datos en JavaScript.
+
+//TODO: Tipo de datos string
+var nombre = "Carlos";
+console.log(nombre);
+
+nombre = 10.5;
+console.log(typeof nombre);
+
+/*
+Nota: con typeof nos dice q tipo de variable es.
+*/
+
+//TODO: Tipo de dato numerico
+var numero = 1000;
+console.log(numero);
+
+//TODO: Tipo de dato Objeto
+var objeto = {
+    nombre : "Juan",
+    apallido : "Perez",
+    telefono : "5892222"
+}
+console.log(objeto);
+
+//TODO: Tipo de datos boolean (true, false)
+var bandera = true;
+console.log(bandera);
+
+//TODO: Tipo de dato function...
+function miFuncion(){}
+console.log(miFuncion);
+
+//TODO: Tipo de dato symbol
+//var simbolo = symbol("mi simbolo");
+console.log(typeof simbolo);
+
+//TODO: Tipo de clase es una funcion
+/*class Persona{
+    constructor(nombre, apellido){
+      this.nombre = nombre;
+      this.apellido = apellido;
+    }
+}
+console.log(typeof Persona); */
+
+//TODO: Tipo undefined
+var x;
+console.log(typeof x);
+
+//TODO: Tipo null = ausencia de valores para valor
+var y = null;
+console.log(typeof y);
+
+//TODO: Tipo array 
+var autos = ['BMW', 'Audi', 'Volvo'];
+console.log(typeof autos);
+
+var z = '';
+console.log(z);
+console.log(typeof z);
---
+++ Javascript/concatenacion de cadenas.js
@@ -0,0 +1,11 @@
+var nombre = 'Juan';
+var apellido = 'Perez';
+
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+var nombreCompleto2 = 'Carlos' + ' '+ 'Lara';
+console.log(nombreCompleto2);
+
+var x = nombre + 219;
+console.log(x);
